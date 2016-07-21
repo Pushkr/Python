e0945e20c1c124995d8705a8b95ffe1270eac782
PUSHKAR GUJAR <pushkarvgujar@gmail.com>
2016-07-21 01:12:07 -0400

    Correction after unittest and PEP 8 compliance

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -1,37 +1,33 @@
-# Write a password generator in Python. Be creative with how you generate passwords - 
-# strong passwords have a mix of lowercase letters, uppercase letters, numbers, and symbols. 
-# The passwords should be random, generating a new password every time the user asks for a new password. 
+# Write a password generator in Python. Be creative with how you generate passwords -
+# strong passwords have a mix of lowercase letters, uppercase letters, numbers, and symbols.
+# The passwords should be random, generating a new password every time the user asks for a new password.
 # Include your run-time code in a main method.
-# 
+#
 # Extra:
-# 
+#
 # Ask the user how strong they want their password to be. For weak passwords, pick a word or two from a list.
 
 import random
 import string
 
-# Below solution have two type of implementation. First is using function calls GetWeakerPassword and GetStrongerOne.
-# Second solution consist of class implementation where, user can set the preferences for password by calling various INCLUDE 
-#methods 
 
-
-def GetWeakerPassword():
-    WeakPassList = ['password123', 'password1980', '12345678', '123456', 'baseball',
+def getweakerpassword():
+    weakpasslist = ['password123', 'password1980', '12345678', '123456', 'baseball',
                     'football', 'cricket', 'trustno1', 'batman', 'abc123', '000000']
-    return WeakPassList[random.choice(range(0, len(WeakPassList)))]
+    return weakpasslist[random.choice(range(0, len(weakpasslist)))]
 
 
-def GetStrongOne(PassLen):
-    LowerAlpha = string.ascii_lowercase
-    UpperAlpha = string.ascii_uppercase
-    Digits = string.digits
-    SpecialChar = string.punctuation
-    FullList = (list(LowerAlpha) + list(UpperAlpha) + list(Digits) + list(SpecialChar))
-    pass1 = ''.join(random.sample(FullList, PassLen))
+def getstrongone(passlen):
+    loweraplha = string.ascii_lowercase
+    upperaplha = string.ascii_uppercase
+    digits = string.digits
+    specialchar = string.punctuation
+    fulllist = (list(loweraplha) + list(upperaplha) + list(digits) + list(specialchar))
+    pass1 = ''.join(random.sample(fulllist, passlen))
     print(pass1)
 
 
-class SetPreferences():
+class SetPreferences:
     FullList = list(string.ascii_lowercase)
 
     def __init__(self):
@@ -39,61 +35,48 @@ class SetPreferences():
         self.upperaplha = True
         self.digits = True
         self.specialchar = True
-        self.passlen = 8   # Default password length
+        self.passlen = 8  # default
 
     def setlength(self, passlen):
         self.passlen = passlen
-        print("Password length set to %d" % self.passlen)
+        # print("Password length set to %d" % self.passlen)
 
     def getlength(self):
         return self.passlen
 
-    def IncludeUpper(self,choice):
+    def includeupper(self, choice):
         self.upperaplha = choice
 
-    def IncludeDigit(self,choice):
+    def includedigit(self, choice):
         self.digits = choice
 
-    def IncludeSpecial(self,choice):
+    def includespecial(self, choice):
         self.specialchar = choice
 
-    def Finalize(self):
-        # FullList = list(string.ascii_lowercase)
-        if self.specialchar == True:
+    # def getPreferences(self):
+    #     print("Lowercase: %s \n Uppercase : %s \n Digits : %s \n Special : %s",%(self.loweraplha,self.upperaplha,
+    #     self.digits,self.specialchar))
+
+    def finalize(self):
+        self.FullList = list(string.ascii_lowercase)
+        # if self.loweraplha == True:
+        #     self.FullList = list(string.ascii_lowercase)
+        if self.specialchar is True:
             self.FullList += list(string.punctuation)
-        if self.digits == True:
+        if self.digits is True:
             self.FullList += list(string.digits)
-        if self.upperaplha == True:
+        if self.upperaplha is True:
             self.FullList += list(string.ascii_uppercase)
 
 
+# noinspection PyMissingConstructor,PyPep8Naming,PyShadowingNames
 class NewPassword(SetPreferences):
-
-    def __init__(self,SetPreferences):
-        self.passlen=SetPreferences.passlen
+    def __init__(self, SetPreferences):
+        SetPreferences.finalize()
+        self.passlen = SetPreferences.passlen
         self.FullList = SetPreferences.FullList
-        SetPreferences.Finalize() 
-        
+
     def getpassword(self):
-        pass1 = ''.join(random.sample(self.FullList,self.passlen))
+        pass1 = ''.join(random.sample(self.FullList, self.passlen))
         return pass1
 
-#Solution using function calls
-print(GetWeakerPassword())
-PassLen = int(input("Enter desired password length:"))
-GetStrongOne(PassLen)
-
-
-#Solution using class implementation
-NewPref = SetPreferences()
-NewPref.IncludeDigit(True)
-NewPref.IncludeUpper(False)
-NewPref.IncludeSpecial(False)
-NewPref.setlength(12)
-PassWord = NewPassword(NewPref) ;
-print(PassWord.getpassword())
-
-# using default settings of preferences.
-s = SetPreferences()
-new1 = NewPassword(s)
-print(new1.getpassword())
